5f8e7a8be7ae718577da79e637277011d2580f5e
Aditya-IITM-BS <adityabaxla@study.iitm.ac.in>
2025-09-02 12:43:24 +0530

    p4 main changed

--- 4-MailMerge/main.c
+++ 4-MailMerge/main.c
@@ -1,45 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "helpers.h"
+
+// Function to open a file safely, return the file pointer
+FILE *open_file(const char *filename, const char *mode)
+{
+}
+
+// Function to generate output filename for employee
+void generate_output_filename(char *filename, const char *id)
+{
+}
+
+// Function to process one employee and generate payslip
+void process_employee(const char *id, const char *name, const char *dept, const char *salary)
+{
+    char filename[100];
+    char template_line[1024], output_line[1024];
+
+    // Create output file name like payslip_101.txt using generate_output_filename function
+
+    // Open output file
+
+    // Open template file
+
+    // Read template line by line
+    while (fgets(template_line, sizeof(template_line), templateFile))
+    {
+    }
+
+    // Close open files
+}
+
+// Function to read employees.txt and process each record
+void process_all_employees()
+{
+    // use open_file function to open employees.txt in read mode
+    FILE *empFile = open_file("employees.txt", "r");
+    char id[20], name[50], dept[50], salary[20];
+
+    // read id, name, dept, salary line by line and call process_employee function with required arguments
+
+    // close employees.txt file
+    fclose(empFile);
+}
 
 int main()
 {
-    FILE *empFile = fopen("employees.txt", "r");
-    FILE *templateFile;
-    char id[20], name[50], dept[50], salary[20];
-    char template_line[1024], output_line[1024];
-
-    if (!empFile)
-    {
-        printf("Error: employees.txt not found!\n");
-        return 1;
-    }
-
-    while (fscanf(empFile, "%s %s %s %s", id, name, dept, salary) != EOF)
-    {
-        // Create output file for employee
-        char filename[100];
-        sprintf(filename, "payslip_%s.txt", id);
-        FILE *outFile = fopen(filename, "w");
-
-        templateFile = fopen("template.txt", "r");
-        if (!templateFile)
-        {
-            printf("Error: template.txt not found!\n");
-            return 1;
-        }
-
-        while (fgets(template_line, sizeof(template_line), templateFile))
-        {
-            trim_newline(template_line);
-            replace_placeholder(template_line, output_line, id, name, dept, salary);
-            fprintf(outFile, "%s\n", output_line);
-        }
-
-        fclose(templateFile);
-        fclose(outFile);
-    }
-
-    fclose(empFile);
+    process_all_employees();
+    printf("Payslips generated successfully!\n");
     return 0;
 }
